d54a598657d7bda3edb7cb604214e4c980415d03
Jason Aden <jasonaden@gmail.com>
2015-08-23 15:03:01 -0700

    feat(summary): Add summary component for displaying prices.

---
+++ client/components/summary.js
@@ -0,0 +1,21 @@
+angular.module('app').directive('summary', function () {
+	return {
+		templateUrl: 'client/components/summary.html',
+		controller: SummaryController,
+		controllerAs: 'summary',
+		scope: {stock: '=data'},
+		bindToController: true
+	}
+});
+
+function SummaryController () {
+	
+}
+
+SummaryController.prototype.isNegative = function () {
+	return !(!this.stock || this.stock.change >= 0);
+}
+
+SummaryController.prototype.isPositive = function () {
+	return !(!this.stock || this.stock.change <= 0);
+}
