2f76c5d362784ac2319436d06731c2ee4174bfb9
Jason Aden <jasonaden@gmail.com>
2015-08-23 15:00:23 -0700

    chore(routes): Enable HTML5 mode for URL.

--- client/routes.js
+++ client/routes.js
@@ -1,1 +1,17 @@
-angular.module('app')
+angular.module('app').config(ConfigureRoute);
+
+function ConfigureRoute ($routeProvider, $locationProvider) {
+	
+	$locationProvider.html5Mode(true);
+	
+	$routeProvider
+	.when('/dashboard', {
+		controller: 'Dashboard as dashboard',
+		templateUrl: 'client/dashboard/index.html'
+	})
+	.when('/manage', {
+		controller: 'Manage as manage',
+		templateUrl: 'client/manage/index.html'
+	})
+	.otherwise('/dashboard');
+}
